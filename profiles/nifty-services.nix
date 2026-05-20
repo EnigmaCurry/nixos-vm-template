b56bcd349c48b6b082f5f20aa4947cf481a451da
EnigmaCurry <ryan@enigmacurry.com>
2026-05-20 06:35:56 +0000

    update traefik cert SAN to nifty.internal domain

--- profiles/nifty-services.nix
+++ profiles/nifty-services.nix
@@ -30,7 +30,7 @@
     services.nifty-services.service-monitor.routerUrl = "https://10.99.2.1:3000";
     services.nifty-services.service-monitor.package = nifty-filter.packages.${pkgs.stdenv.hostPlatform.system}.nifty-service-monitor;
     services.nifty-services.traefik.dashboard.enable = true;
-    services.nifty-services.traefik.cert.san = [ "DNS:infra.lan" "IP:10.99.2.10" ];
+    services.nifty-services.traefik.cert.san = [ "DNS:nifty.internal" "IP:10.99.2.2" ];
     # Service routing and access control is managed dynamically by the
     # service-monitor from the HCL config's services.traefik block.
 
