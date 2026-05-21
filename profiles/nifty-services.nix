51e19a67821dc76aba1738a59583a18cb0c4d778
EnigmaCurry <ryan@enigmacurry.com>
2026-05-21 02:30:35 +0000

    update service-monitor router URL from port 3000 to 443

--- profiles/nifty-services.nix
+++ profiles/nifty-services.nix
@@ -27,7 +27,7 @@
     services.nifty-services.technitium.enable = true;
     services.nifty-services.traefik.enable = true;
     services.nifty-services.service-monitor.enable = true;
-    services.nifty-services.service-monitor.routerUrl = "https://10.99.2.1:3000";
+    services.nifty-services.service-monitor.routerUrl = "https://10.99.2.1";
     services.nifty-services.service-monitor.package = nifty-filter.packages.${pkgs.stdenv.hostPlatform.system}.nifty-service-monitor;
     services.nifty-services.traefik.dashboard.enable = true;
     services.nifty-services.traefik.cert.san = [ "DNS:nifty.internal" "IP:10.99.2.2" ];
